81dbf03991b31c0cb016a824f10c30093dd61843
Cedric <cedric.marechal1@gmail.com>
2019-10-24 14:22:18 +0200

    MemberDto and CreateMemberDto created

---
+++ domain/src/main/java/com/testingtigers/domain/dtos/MemberDto.java
@@ -0,0 +1,89 @@
+package com.testingtigers.domain.dtos;
+
+public class MemberDto {
+
+    private String INSS;
+    private String id;
+    private String emailAdress;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String postalCode;
+    private String streetName;
+    private String streetNumber;
+
+    public MemberDto() {
+    }
+
+    public String getINSS() {
+        return INSS;
+    }
+
+    public void setINSS(String INSS) {
+        this.INSS = INSS;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+}
