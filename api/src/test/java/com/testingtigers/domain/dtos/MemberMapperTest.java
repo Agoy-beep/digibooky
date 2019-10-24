bbe435075877feaa2c700ec70e4e6e39ea3cacb1
Cedric <cedric.marechal1@gmail.com>
2019-10-24 15:41:51 +0200

    added membermapper unit test

---
+++ api/src/test/java/com/testingtigers/domain/dtos/MemberMapperTest.java
@@ -0,0 +1,21 @@
+package com.testingtigers.domain.dtos;
+
+import com.testingtigers.domain.users.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberMapperTest {
+    @Test
+    void givenMemberWhenConvertGetProperDto() {
+        //GIVEN
+        MemberMapper memberMapper = new MemberMapper();
+        Member member = new Member("7514","hah@gmail.com", "hah","han");
+        //WHEN
+        MemberDto memberDto = memberMapper.convertMemberToDto(member);
+        //THEN
+        Assertions.assertThat(memberDto.getFirstName()).isEqualTo(member.getFirstName());
+        Assertions.assertThat(memberDto.getPostalCode()).isEqualTo("null");
+    }
+}
