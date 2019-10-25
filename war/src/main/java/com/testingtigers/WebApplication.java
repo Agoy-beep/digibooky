5249652aa474041a69c743728e036dfe6a51deb0
Agoy-beep <arnegoyvaerts@gmail.com>
2019-10-25 13:08:58 +0200

    Added swagger, edited pom and made minor spelling changes in BookService

--- war/src/main/java/com/testingtigers/WebApplication.java
+++ war/src/main/java/com/testingtigers/WebApplication.java
@@ -3,8 +3,10 @@ package com.testingtigers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.testingtigers")
+@EnableSwagger2
 public class WebApplication {
 
     public static void main(String[] args) {
