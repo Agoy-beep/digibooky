645fc3aa24f0762a4204640bdce3e916708a1061
DoebiMe <dannydoubbel@gmail.com>
2019-10-25 10:28:35 +0200

    findISBN findTitle
    added in controller
    added in repostitory

--- domain/src/main/java/com/testingtigers/domain/repositories/BookDataBaseDummy.java
+++ domain/src/main/java/com/testingtigers/domain/repositories/BookDataBaseDummy.java
@@ -1,22 +1,29 @@
 package com.testingtigers.domain.repositories;
 
+import com.testingtigers.domain.Author;
 import com.testingtigers.domain.Book;
+import com.yevdo.jwildcard.JWildcard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class BookDataBaseDummy {
 
     private final Map<String, Book> bookDB;
 
-
-    public BookDataBaseDummy(Map<String, Book> bookDB) {
-        this.bookDB = bookDB;
+    public BookDataBaseDummy() {
+        this.bookDB = new HashMap<>();
     }
 
     public Map<String, Book> getBookDB() {
         return bookDB;
     }
+
 }
